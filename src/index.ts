b306919036f49dc692495e1377c7ecaef07fb67b
Sebastian Frysztak <sebfry@gmail.com>
2019-01-04 10:55:08 +0100

    implement GET /accessToken resource

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,49 @@
 require("dotenv").config();
 
 import express from "express";
+import fetch from "node-fetch";
+import { Dropbox } from "dropbox";
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("helloooo");
+
+var env = process.env.NODE_ENV || "development";
+if (env === "development") {
+  app.use((req, res, next) => {
+    res
+      .header("Access-Control-Allow-Origin", "http://localhost:3000")
+      .header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+}
+
+// see https://github.com/dropbox/dropbox-sdk-js/issues/221
+const dbx = new Dropbox({
+  clientId: process.env.DROPBOX_KEY,
+  clientSecret: process.env.DROPBOX_SECRET,
+  fetch: fetch
+} as any);
+
+app.get("/accessToken", async (req, res) => {
+  const authorizationCode: string = req.query.authorizationCode;
+  if (authorizationCode) {
+    const redirectUrl = process.env.DROPBOX_REDIRECT_URL as string;
+    try {
+      const accessCode = await dbx.getAccessTokenFromCode(
+        redirectUrl,
+        authorizationCode
+      );
+
+      res
+        .cookie("dbxAccessToken", accessCode, {
+          expires: new Date("Fri, 31 Dec 9999 23:59:59 GMT")
+        })
+        .send();
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  } else {
+    res.status(401).send("No authorization code");
+  }
 });
 
 app.listen(3333, () => {
